843be6b1afdb45d98060b02f31e1ac3361a9af7d
Eric Stern <eric@ericstern.com>
2015-09-04 13:08:55 -0700

    Make the HTTPMethod enum string-castable

--- src/Enums/HTTPMethod.php
+++ src/Enums/HTTPMethod.php
@@ -9,9 +9,14 @@ class HTTPMethod extends Enum
 
     // Other methods exist, but these are the only relevant ones for RESTful
     // APIs
-    const GET = 'get';
-    const POST = 'post';
-    const PUT = 'put';
-    const DELETE = 'delete';
+    const GET = 'GET';
+    const POST = 'POST';
+    const PUT = 'PUT';
+    const DELETE = 'DELETE';
+
+    public function __toString()
+    {
+        return $this->getValue();
+    }
 
 }
