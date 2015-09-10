f7dcae3ce7c2f322ac88f73afa2a37eeaf7e2386
Eric Stern <eric@ericstern.com>
2015-09-10 15:16:35 -0700

    WIP: Dispatcher for use in front-controller

---
+++ src/Dispatcher.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Firehed\API;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Firehed\Common\ClassMapper;
+use Firehed\Input\Containers\ParsedInput;
+use Firehed\Input\Exceptions\InputException;
+
+use Zend\Diactoros\Response\JsonResponse;
+
+class Dispatcher
+{
+
+    private $endpoint_list;
+    private $error_handler;
+    private $parser_list;
+    private $request;
+
+    /**
+     * Configure a callback when an error condition occurs. ... more info
+     * coming
+     *
+     *
+     * @param callable error handler
+     * @return self
+     *
+    public function setErrorHandler(callable $handler)
+    {
+        $this->error_handler = $handler;
+        return $this;
+    }
+    *** Not useful yet ***/
+
+    /**
+     * Inject the request
+     *
+     * @param ServerRequestInterface The request
+     * @return self
+     */
+    public function setRequest(ServerRequestInterface $request)
+    {
+        $this->request = $request;
+        return $this;
+    }
+
+    /**
+     * Set the parser list. Can be an array consumable by ClassMapper or
+     * a string representing a file parsable by same. The list must map
+     * MIME-types to Firehed\Input\ParserInterface class names.
+     *
+     * @param array|string The parser list or its path
+     * @return self
+     */
+    public function setParserList($parser_list)
+    {
+        $this->parser_list = $parser_list;
+        return $this;
+    }
+
+    public function setEndpointList($endpoint_list)
+    {
+        $this->endpoint_list = $endpoint_list;
+        return $this;
+    }
+
+    /**
+     * @param int HTTP status code
+     * @return Psr\Http\Message\ResponseInterface
+     */
+    protected function error($http_code)
+    {
+        return new JsonResponse([
+            'error' => [
+                'message' => 'MESSAGE',
+            ],
+        ], $http_code);
+    }
+
+    public function dispatch()
+    {
+        if (!$this->request || !$this->parser_list || !$this->endpoint_list) {
+            return $this->error(500);
+        }
+
+        $data = [];
+        // Presence of Content-type header indicates PUT/POST; parse it. We
+        // don't use $_POST because additional content types are supported.
+        $cth = $this->request->getHeader('Content-type');
+        if ($cth) {
+            list($parser_class) = (new ClassMapper($this->parser_list))
+                ->search($cth[0]);
+            if (!$parser_class) {
+                // 400 Bad Request
+                return $this->error(400);
+            }
+            $parser = new $parser_class;
+            $data = $parser->parse($this->request->getBody());
+        }
+
+        $parsed_input = new ParsedInput($data);
+
+        list($class, $uri_data) = (new ClassMapper($this->endpoint_list))
+            ->filter(strtoupper($this->request->getMethod()))
+            ->search($this->request->getUri()->getPath());
+        if (!$class) {
+            // Handle error: 404
+            return $this->error(404);
+        }
+        $parsed_input->addData(new ParsedInput($uri_data));
+
+        $endpoint = new $class;
+        try {
+            $safe_input = $parsed_input->validate($endpoint);
+        }
+        catch (InputException $e) {
+            return $this->error(400);
+        }
+        // something something middleware
+        return $endpoint->execute($safe_input);
+
+    }
+}
---
+++ tests/DispatcherTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Firehed\API;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @coversDefaultClass Firehed\API\Dispatcher
+ * @covers ::<protected>
+ * @covers ::<private>
+ */
+class DispatcherTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testConstruct()
+    {
+        $this->assertInstanceOf('Firehed\API\Dispatcher',
+            new Dispatcher());
+    }
+
+
+    public function testNoRouteMatchReturns404()
+    {
+        $this->i();
+    }
+
+    /** @covers ::dispatch */
+    public function testDispatchReturns500WhenMissingData()
+    {
+        $d = new Dispatcher();
+        $ret = $d->dispatch();
+        $this->checkResponse($ret, 500);
+    }
+
+
+    /** @covers ::setEndpointList */
+    public function testSetEndpointListReturnsSelf()
+    {
+        $d = new Dispatcher();
+        $this->assertSame($d,
+            $d->setEndpointList('list'),
+            'setEndpointList did not return $this');
+    }
+
+    /** @covers ::setParserList */
+    public function testSetParserListReturnsSelf()
+    {
+        $d = new Dispatcher();
+        $this->assertSame($d,
+            $d->setParserList('list'),
+            'setParserList did not return $this');
+    }
+
+    /** @covers ::setRequest */
+    public function testSetRequestReturnsSelf()
+    {
+        $d = new Dispatcher();
+        $req = $this->getMock('Psr\Http\Message\ServerRequestInterface');
+        $this->assertSame($d,
+            $d->setRequest($req),
+            'setRequest did not return $this');
+    }
+
+
+
+
+
+    function i() {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @param ResponseInterface response to test
+     * @param int HTTP status code to check for
+     */
+    private function checkResponse(ResponseInterface $response, $expected_code)
+    {
+        $this->assertSame($expected_code,
+            $response->getStatusCode(),
+            'Incorrect status code in response');
+    }
+
+}
