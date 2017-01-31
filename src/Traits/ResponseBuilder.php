0b3f20005af1c889e44b0f1a839850963843c010
Eric Stern <eric@ericstern.com>
2017-01-30 22:12:01 -0800

    Add response builder trait

---
+++ src/Traits/ResponseBuilder.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace Firehed\API\Traits;
+
+use Psr\Http\Message\ResponseInterface;
+use Zend\Diactoros\Response;
+
+/**
+ * Provides simple response-building methods for HTML, JSON, and plain text
+ * bodies.
+ */
+trait ResponseBuilder
+{
+
+    /**
+     * Returns an empty-body response
+     *
+     * @param int $code HTTP Status code (deafult 204)
+     *
+     * @return ResponseInterface a PSR-7 response
+     */
+    public function emptyResponse(int $code = 204): ResponseInterface
+    {
+        return new Response\EmptyResponse($code);
+    }
+
+    /**
+     * Treats the body as HTML and builds a response with an HTML Content-type
+     * header
+     *
+     * @param string $body The HTML to send
+     * @param int $code HTTP Status code (deafult 200)
+     *
+     * @return ResponseInterface a PSR-7 response
+     */
+    public function htmlResponse(string $body, int $code = 200): ResponseInterface
+    {
+        return new Response\HtmlResponse($body, $code);
+    }
+
+    /**
+     * JSON-encodes the provided data and builds a response with with a JSON
+     * Content-type header
+     *
+     * @param mixed $data Any JSON-encodable data
+     * @param int $code HTTP Status code (deafult 200)
+     *
+     * @return ResponseInterface a PSR-7 response
+     */
+    public function jsonResponse($data, int $code = 200): ResponseInterface
+    {
+        return new Response\JsonResponse($data, $code);
+    }
+
+    /**
+     * Builds a plaintext response with the provided string
+     *
+     * @param string $body The text to send
+     * @param int $code HTTP Status code (deafult 200)
+     *
+     * @return ResponseInterface a PSR-7 response
+     */
+    public function textResponse(string $body, int $code = 200): ResponseInterface
+    {
+        return new Response\TextResponse($body, $code);
+    }
+
+}
---
+++ tests/Traits/ResponseBuilderTest.php
@@ -0,0 +1,119 @@
+<?php
+declare(strict_types=1);
+
+namespace Firehed\API\Traits;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @coversDefaultClass Firehed\API\Traits\ResponseBuilder
+ * @covers ::<protected>
+ * @covers ::<private>
+ */
+class ResponseBuilderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @covers ::emptyResponse
+     */
+    public function testEmptyResponse()
+    {
+        $impl = new class { use ResponseBuilder; };
+
+        $code = random_int(400, 417);
+        $response = $impl->emptyResponse($code);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEmpty($response->getHeaderLine('content-type'),
+            'Content-type header');
+        $this->assertSame($code,
+            $response->getStatusCode(),
+            'Status code was not set');
+        $this->assertEmpty((string)$response->getBody(),
+            'Body was not empty');
+    }
+
+    /**
+     * @covers ::htmlResponse
+     */
+    public function testHtmlResponse()
+    {
+        $impl = new class { use ResponseBuilder; };
+
+        $code = random_int(400, 417);
+        $html = '<html><body><p>Hi</p></body></html>';
+        $response = $impl->htmlResponse($html, $code);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame('text/html; charset=utf-8',
+            $response->getHeaderLine('content-type'),
+            'Wrong content-type header');
+        $this->assertSame($code,
+            $response->getStatusCode(),
+            'Status code was not set');
+        $this->assertSame($html,
+            (string)$response->getBody(),
+            'Body was rendered incorrectly');
+    }
+
+    /**
+     * @covers ::jsonResponse
+     * @dataProvider jsonData
+     */
+    public function testJsonResponse($data)
+    {
+        $impl = new class { use ResponseBuilder; };
+
+        $code = random_int(400, 417);
+        $response = $impl->jsonResponse($data, $code);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame('application/json',
+            $response->getHeaderLine('content-type'),
+            'Wrong content-type header');
+        $this->assertSame($code,
+            $response->getStatusCode(),
+            'Status code was not set');
+        $this->assertSame(json_encode($data),
+            (string)$response->getBody(),
+            'Body was rendered incorrectly');
+    }
+
+    /**
+     * @covers ::textResponse
+     */
+    public function testtextResponse()
+    {
+        $impl = new class { use ResponseBuilder; };
+
+        $code = random_int(400, 417);
+        $text = 'Hello, world!';
+        $response = $impl->textResponse($text, $code);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame('text/plain; charset=utf-8',
+            $response->getHeaderLine('content-type'),
+            'Wrong content-type header');
+        $this->assertSame($code,
+            $response->getStatusCode(),
+            'Status code was not set');
+        $this->assertSame($text,
+            (string)$response->getBody(),
+            'Body was rendered incorrectly');
+    }
+
+
+    public function jsonData(): array
+    {
+        return [
+            [1],
+            [false],
+            [null],
+            ['some string'],
+            [[1,2,3]],
+            [[true, false]],
+            [['a', 'b', 'c']],
+            [['key' => 'value']],
+        ];
+    }
+
+}
