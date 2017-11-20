5f711e252efd441c165eef8e27c899fb7b29fbf3
Eric Stern <eric@ericstern.com>
2017-11-20 12:05:46 -0800

    Add class-level documentation to the endpoint interface

--- src/Interfaces/EndpointInterface.php
+++ src/Interfaces/EndpointInterface.php
@@ -15,6 +15,16 @@ use Psr\Http\Message\ {
     ResponseInterface as Response
 };
 
+/**
+ * Interface for implementing an endpoint in your application's API. All
+ * classes in the project implementing this interface will be auto-detected at
+ * build time (by `generate_endpoint_list`) and made available.
+ *
+ * If the implementing class has a constructor with parameters, the class MUST
+ * be configuired with a container injected into the Dispatcher. If there are
+ * no paramters on the constructor (or there is no constructor), that is
+ * optional.
+ */
 interface EndpointInterface extends ValidationInterface
 {
 
