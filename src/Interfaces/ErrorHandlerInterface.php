29a86e81ea52c8d84ce7ef3c89051a6d16b82ee9
Eric Stern <eric@ericstern.com>
2018-05-20 17:58:21 -0700

    Change doc comment for clarity

--- src/Interfaces/ErrorHandlerInterface.php
+++ src/Interfaces/ErrorHandlerInterface.php
@@ -12,7 +12,7 @@ interface ErrorHandlerInterface
     /**
      * Handle an exception for the given request. It is RECOMMENDED that
      * implementations use the $request's accept header to determine how best
-     * to build the response.
+     * to format the response.
      */
     public function handle(ServerRequestInterface $request, Throwable $t): ResponseInterface;
 }
