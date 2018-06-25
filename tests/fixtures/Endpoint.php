54f1f442d549863e045b14a760c5f7d154fbe644
Eric Stern <eric@ericstern.com>
2018-06-24 20:41:53 -0700

    Add endpoint fixture (missed add)

---
+++ tests/fixtures/Endpoint.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Firehed\API\fixtures;
+
+use Firehed\API\Interfaces\EndpointInterface;
+use Firehed\API\Traits\Authentication;
+use Firehed\API\Traits\Input;
+use Firehed\API\Traits\Request;
+use Firehed\Input\Containers\SafeInput;
+use Firehed\InputObjects;
+use Psr\Http\Message\ResponseInterface;
+use Zend\Diactoros\Response\EmptyResponse;
+
+class Endpoint implements EndpointInterface
+{
+    use Authentication\None;
+    use Input\NoOptional;
+    use Input\NoRequired;
+    use Request\Options;
+
+    public function handleException($e): ResponseInterface
+    {
+    }
+
+    public function getUri(): string
+    {
+        return '/.*';
+    }
+
+    public function execute(SafeInput $input): ResponseInterface
+    {
+        return (new EmptyResponse())
+            ->withHeader(
+                'Access-Control-Allow-Headers',
+                'Authorization, Content-type'
+            );
+    }
+}
