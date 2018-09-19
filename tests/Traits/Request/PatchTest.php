01420e9b905137dc67b5f501de9fcc42f8e944c8
Eric Stern <eric@ericstern.com>
2018-09-19 14:21:20 -0700

    Add PATCH support

---
+++ tests/Traits/Request/PatchTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Firehed\API\Traits\Request;
+
+/**
+ * @coversDefaultClass Firehed\API\Traits\Request\Patch
+ * @covers ::<protected>
+ * @covers ::<private>
+ */
+class PatchTest extends \PHPUnit\Framework\TestCase
+{
+
+    /**
+     * @covers ::getMethod
+     */
+    public function testGetMethod()
+    {
+        $obj = new class {
+            use Patch;
+        };
+        $this->assertEquals(
+            \Firehed\API\Enums\HTTPMethod::PATCH(),
+            $obj->getMethod(),
+            'getMethod did not return HTTP PATCH'
+        );
+    }
+}
