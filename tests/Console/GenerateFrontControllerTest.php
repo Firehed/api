ac81df9d36353768b4723a6bb61e40baafd5f115
Eric Stern <eric@ericstern.com>
2019-01-02 21:40:02 -0800

    test gen fc with config file

--- tests/Console/GenerateFrontControllerTest.php
+++ tests/Console/GenerateFrontControllerTest.php
@@ -14,16 +14,10 @@ use Symfony\Component\Console\Tester\CommandTester;
  */
 class GenerateFrontControllerTest extends \PHPUnit\Framework\TestCase
 {
-    private $config;
     private $oldFrontController;
 
     public function setUp()
     {
-        $this->config = new Config([
-            'namespace' => 'Firehed\API',
-            'source' => 'src',
-            'webroot' => 'public',
-        ]);
         if (file_exists('public/index.php')) {
             $this->oldFrontController = tempnam(sys_get_temp_dir(), 'phpunit_fc_');
             rename('public/index.php', $this->oldFrontController);
@@ -43,17 +37,44 @@ class GenerateFrontControllerTest extends \PHPUnit\Framework\TestCase
     /** @covers ::__construct */
     public function testConstruct()
     {
+        /** @var Config */
         $config = $this->createMock(Config::class);
-        $this->assertInstanceOf(Command::class, new GenerateFrontController($this->config));
+        $this->assertInstanceOf(Command::class, new GenerateFrontController($config));
     }
 
     public function testExecute()
     {
-        $command = new GenerateFrontController($this->config);
+        $config = new Config([
+            Config::KEY_NAMESPACE => 'Firehed\API',
+            Config::KEY_SOURCE => 'src',
+            Config::KEY_WEBROOT => 'public',
+        ]);
+        $command = new GenerateFrontController($config);
         $tester = new CommandTester($command);
         $tester->execute([]);
         $file = file_get_contents('public/index.php');
         $lines = explode("\n", $file);
         $this->assertSame('<?php', $lines[0], 'Output didn\'t start with a PHP tag');
+    }
+
+    public function testExecuteWithContainer()
+    {
+        $config = new Config([
+            Config::KEY_CONTAINER => __DIR__ . '/config.php',
+            Config::KEY_NAMESPACE => 'Firehed\API',
+            Config::KEY_SOURCE => 'src',
+            Config::KEY_WEBROOT => 'public',
+        ]);
+        $command = new GenerateFrontController($config);
+        $tester = new CommandTester($command);
+        $tester->execute([]);
+        $file = file_get_contents('public/index.php');
+        $lines = explode("\n", $file);
+        $this->assertSame('<?php', $lines[0], 'Output didn\'t start with a PHP tag');
+
+        $this->assertNotFalse(
+            strpos($file, __DIR__ . '/config.php'),
+            'Config file not set in output'
+        );
     }
 }
