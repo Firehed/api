db9dd23f8839c35ecaedd50a0cb0f63642fb614f
Eric Stern <eric@ericstern.com>
2021-06-10 13:03:02 -0700

    Conditional enum definition based on php version

--- src/Enums/HTTPMethod.php
+++ src/Enums/HTTPMethod.php
@@ -4,35 +4,13 @@ declare(strict_types=1);
 
 namespace Firehed\API\Enums;
 
-use Firehed\Common\Enum;
+use function class_alias;
+use function version_compare;
 
-/**
- * Direct use of this class (via these static methods) is deprecated. Instead,
- * implementations should rely on the Request traits.
- *
- * This will be converted to a native Enum in PHP 8.1.
- *
- * @method static HTTPMethod DELETE()
- * @method static HTTPMethod GET()
- * @method static HTTPMethod OPTIONS()
- * @method static HTTPMethod PATCH()
- * @method static HTTPMethod POST()
- * @method static HTTPMethod PUT()
- */
-class HTTPMethod extends Enum
-{
+use const PHP_VERSION;
 
-    // Other methods exist, but these are the only relevant ones for RESTful
-    // APIs
-    const GET = 'GET';
-    const PATCH = 'PATCH';
-    const POST = 'POST';
-    const PUT = 'PUT';
-    const DELETE = 'DELETE';
-    const OPTIONS = 'OPTIONS';
-
-    public function __toString()
-    {
-        return $this->getValue();
-    }
+if (version_compare(PHP_VERSION, '8.1.0') >= 0) {
+    class_alias(HTTPMethodGTE81::class, HTTPMethod::class);
+} else {
+    class_alias(HTTPMethodLTE80::class, HTTPMethod::class);
 }
