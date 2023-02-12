c4b31b3b24f4d9cca6a5aae65d00b632bcec60a0
Eric Stern <eric@ericstern.com>
2023-02-12 11:06:16 -0800

    Disable PSR on builtin container

--- src/Container.php
+++ src/Container.php
@@ -8,8 +8,10 @@ use Psr\Container as Psr;
 /**
  * Ultra simple array wrapper for a PSR container. No closures, no evaluation,
  * nothing else. Just a dumb-as-rocks key/value store.
+ *
+ * @deprecated This class will be removed in the next version.
  */
-class Container implements Psr\ContainerInterface
+class Container // implements Psr\ContainerInterface
 {
     /** @var array */
     private $data;
